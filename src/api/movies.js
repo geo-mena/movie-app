c635f37e24b12c21101704c544194110bb22d472
Geovanny Mena <geomenacontact@gmail.com>
2023-12-11 18:47:00 -0500

    Update movie API URLs

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const getMovies = () => {
-  return axios.get('https://api-movies-m9vp.onrender.com/movies');
+  return axios.get('https://api-movies-exam.onrender.com/movies');
 };
 
 export const getMovieById = (id) => {
-  return axios.get(`https://api-movies-m9vp.onrender.com/movies/${id}`);
+  return axios.get(`https://api-movies-exam.onrender.com/movies/${id}`);
 };
